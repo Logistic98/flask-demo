67b32a370bb04fe841c4215306e40b11469edbb4
hujun <18660093805@163.com>
2022-08-01 15:21:42 +0800

    使用pre-request优化Flask入参校验

--- img-flask-demo/img-server.py
+++ img-flask-demo/img-server.py
@@ -2,9 +2,9 @@
 
 import os
 from uuid import uuid1
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from flask_cors import CORS
-import json
+from pre_request import pre, Rule
 
 from code import ResponseCode, ResponseMessage
 from log import logger
@@ -14,28 +14,37 @@ from utils import base64_to_img
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
-
 """
 # 方法功能说明
 """
 @app.route(rule='/api/moduleName/methodName', methods=['POST'])
 def methodName():
 
-    # 从请求中解析出图像的base64字符串
-    request_data = request.get_data(as_text=True)
-    request_data = ''.join(request_data.split())
-    request_body = json.loads(request_data)
-    image_b64 = request_body.get("img")
-    if not image_b64:
+    # 参数校验
+    rule = {
+        "img": Rule(type=str, required=True),
+        "ext": Rule(type=str, required=False)
+    }
+    try:
+        params = pre.parse(rule=rule)
+    except Exception as e:
+        logger.error(e)
         fail_response = dict(code=ResponseCode.RARAM_FAIL, msg=ResponseMessage.RARAM_FAIL, data=None)
         logger.error(fail_response)
         return jsonify(fail_response)
+
+    # 获取参数
+    image_b64 = params.get("img")
+    ext = params.get("ext")
 
     # 将base64字符串解析成图片保存
     if not os.path.exists('./img'):
         os.makedirs('./img')
     uuid = uuid1()
-    img_path = './img/{}.jpg'.format(uuid)
+    if ext is not None:
+        img_path = './img/{}.{}'.format(uuid, ext)
+    else:
+        img_path = './img/{}.jpg'.format(uuid)
     try:
         base64_to_img(image_b64, img_path)
     except Exception as e:
@@ -45,7 +54,7 @@ def methodName():
         return jsonify(fail_response)
 
     # 下面对保存的图片进行若干处理
-    result = 'hello world'
+    result = image_b64
     logger.info("测试日志记录")
 
     # 处理完成后删除生成的图片文件
@@ -61,4 +70,4 @@ if __name__ == '__main__':
     # 解决中文乱码问题
     app.config['JSON_AS_ASCII'] = False
     # 启动服务 指定主机和端口
-    app.run(host='0.0.0.0', port=5001, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=5003, debug=False, threaded=True)
